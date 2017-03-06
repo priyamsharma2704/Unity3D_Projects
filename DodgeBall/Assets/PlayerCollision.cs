fe0160e6d4b8750aa10f97d80f1bcd6006ce3133
priyamsharma2704 <priyamsharma2704@gmail.com>
2017-03-06 18:35:36 -0500

    initial commit

---
+++ DodgeBall/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement move;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            //Debug.Log("Hit");
+            move.enabled = false;
+        }
+    }
+}
---
+++ DodgeBall/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+    //public float upForce = 20000f;
+    // Use this for initialization
+	void Start () {
+        //Debug.Log("nfiebofifaf");
+        //rb.AddForce(0, 00, 200);
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")) 
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        
+	
+	}
+}
